69511297a757637568aaa4c4ac386b084ea1b20d
Alexandra Stroia <stroia.alexandra@gmail.com>
2022-04-05 22:15:23 +0200

    Caricamento progetto

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    /** BUTTON SCROLLING **/
+    $("#pulsante1").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 0);
+    });
+
+    $("#pulsante2").click(function () {
+        $('html,body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 0);
+    });
+
+
+    /** FORM VALIDATION **/
+    function validateEmail($email) {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        return emailReg.test($email);
+    }
+
+    $("#submit").click(function () {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var msg = $("#msg").val();
+
+        if (name.length == "") {
+            $("#p1").text("Please enter your name");
+            $("#name").focus();
+            return false;
+        }
+
+        else if (!validateEmail(email)) {
+            console.log(1);
+            $("#p2").text("Please enter a valid email");
+            $("#email").focus();
+            return false;
+        }
+
+        else if (msg.length == "") {
+            $("#p3").text("Please enter your message");
+            $("#msg").focus();
+            return false;
+        }
+
+        else {
+            alert("Thank you for you're message. You'll get a response in 24H!");
+            return true;
+        }
+    })
+
+});
